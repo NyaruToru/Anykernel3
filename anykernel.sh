e348d6f9bae23f13960e2a7573986a4695dbf2d3
Nauru Toru <100704182+NyaruToru@users.noreply.github.com>
2023-09-26 05:24:47 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=RabbitKernel kernel for Poco X3 (NFC) by Telegram@NyaruToru
+kernel.string=RabbitKernel for Poco X3 (NFC) by Telegram@NyaruToru
 do.devicecheck=1
 do.modules=0
 do.systemless=0
